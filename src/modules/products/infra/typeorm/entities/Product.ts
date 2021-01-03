60d52000893a28116bf29d3c0e2cb97f1b43fdb9
Kauã Steliano <stelianok@gmail.com>
2021-01-03 12:22:11 -0300

    Created migrations

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -9,13 +9,18 @@ import {
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity()
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('decimal')
   price: number;
 
+  @Column('int')
   quantity: number;
 
   order_products: OrdersProducts[];
